2a4a6ca92777acdaba1515332f46b99bcb83d5a7
bkingery <brian.kingery@gmail.com>
2013-09-10 16:47:38 -0600

    added a JFrame to the main method

--- p1_model_view/src/p1_model_view/Main.java
+++ p1_model_view/src/p1_model_view/Main.java
@@ -1,10 +1,33 @@
 package p1_model_view;
 
-public class Main {
+import java.awt.event.*;
+import javax.swing.*;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class Main 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Model View");
+		
+		//TODO parse the file and create NetworkModel object from it.
+				//TODO open JFrame that contains a NetworkView object that displays the Network Model.
+		
+		//Create the frame
+		JFrame F = new JFrame("Model View");
 
+		//Set the position and the size of frame's window
+		F.setBounds(100, 100, 300, 400);
+		
+		//Set up quitting on close of window
+		F.addWindowListener(
+				new WindowAdapter()
+				{
+					public void windowClosing(WindowEvent evt)
+					{ System.exit(0); }
+				});
+
+		//F.getContentPane().add(new); //TODO what goes here
+		F.setVisible(true);
 	}
 
 }
