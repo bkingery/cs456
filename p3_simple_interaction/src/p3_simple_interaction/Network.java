0656fb5e5eaa1407daaa0cd46efba490641816cd
bkingery <brian.kingery@gmail.com>
2013-10-20 19:49:33 -0600

    Started p3.

---
+++ p3_simple_interaction/src/p3_simple_interaction/Network.java
@@ -0,0 +1,49 @@
+package p3_simple_interaction;
+
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Network 
+{
+	public static void main(String[] args) 
+	{
+//		NetworkModel.Test();
+		
+		NetworkModel networkModel = null;
+		try {
+			if (args.length == 0)
+				networkModel = new NetworkModel();
+			else if (args.length == 1)
+				networkModel = new NetworkModel(args[0]);
+			else
+			{
+				System.out.println("Provide a single file path.");
+				return;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+			
+		//Create the frame
+		JFrame F = new JFrame("Network");
+
+		//Set the position and the size of frame's window
+		F.setBounds(100, 100, 300, 400);
+		
+		//Set up quitting on close of window
+		F.addWindowListener(
+				new WindowAdapter()
+				{
+					public void windowClosing(WindowEvent evt)
+					{ System.exit(0); }
+				});
+
+		NetworkView networkView = new NetworkView(networkModel);
+		F.getContentPane().add(networkView);
+		F.setVisible(true);
+		F.setFocusable(true);
+		networkView.requestFocusInWindow();
+		
+	}
+
+}
---
+++ p3_simple_interaction/src/p3_simple_interaction/NetworkListener.java
@@ -0,0 +1,7 @@
+package p3_simple_interaction;
+
+public interface NetworkListener {
+
+	void nodeChanged(NetworkNode n);
+
+}
